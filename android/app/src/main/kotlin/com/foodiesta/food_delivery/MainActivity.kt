5e65d5cdc33ad655b1e31056769aba18b752f9f0
Anushkatech5 <guptaanushka024@gmail.com>
2024-06-29 21:13:51 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/foodiesta/food_delivery/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.foodiesta.food_delivery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/home/food_age_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class FoodAgeBody extends StatefulWidget {
+  const FoodAgeBody({super.key});
+
+  @override
+  State<FoodAgeBody> createState() => _FoodAgeBodyState();
+}
+
+class _FoodAgeBodyState extends State<FoodAgeBody> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
